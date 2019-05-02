8a12979d2ab70c4512e9431428858ce257d750e9
Charlie <charlie@Charlies-MacBook-Pro.local>
2019-05-01 22:19:54 -0700

    First commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import {Switch,Route} from 'react-router-dom';
+import logo from './logo.svg';
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Cart from './components/Cart.js';
+import Default from './components/Default.js';
+import Details from './components/Details.js';
+import Navbar from './components/Navbar.js';
+import ProductList from './components/ProductList.js';
+
+class App extends Component{
+  render() {
+  return (
+    <React.Fragment>
+      <Navbar />
+      <Switch>
+        <Route exact path = "/" component = {ProductList} />
+        <Route path = "/details" component = {Details} />
+        <Route path = "/cart" component = {Cart} />
+        <Route component = {Default} />
+      </Switch>
+      </React.Fragment>
+
+    );
+  }
+}
+export default App;
